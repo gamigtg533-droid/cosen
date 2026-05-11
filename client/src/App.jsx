83f1a895ae9b64bf6555f8357712433c588c4020
Ankitarjput07 <ankitrajput15557@gmail.com>
2026-05-11 07:14:31 +0530

    feat: add dynamic document title updater based on current route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
 import Navbar from './components/Navbar';
 import Landing from './pages/Landing';
 import Login from './pages/Login';
@@ -18,9 +19,38 @@ import Messages from './pages/Messages';
 import SellerProfile from './pages/SellerProfile';
 import ProtectedRoute from './components/ProtectedRoute';
 
+function TitleUpdater() {
+  const location = useLocation();
+
+  useEffect(() => {
+    const path = location.pathname;
+    let title = 'Cosen';
+    
+    if (path === '/') title = 'Cosen | Campus Marketplace';
+    else if (path === '/login') title = 'Log in | Cosen';
+    else if (path === '/signup') title = 'Sign up | Cosen';
+    else if (path === '/browse') title = 'Browse Services | Cosen';
+    else if (path === '/services/new') title = 'Post a Service | Cosen';
+    else if (path.startsWith('/services/')) title = 'Service Details | Cosen';
+    else if (path === '/dashboard') title = 'Dashboard | Cosen';
+    else if (path === '/profile') title = 'My Profile | Cosen';
+    else if (path.startsWith('/profile/')) title = 'User Profile | Cosen';
+    else if (path.startsWith('/orders/')) title = 'Order Details | Cosen';
+    else if (path === '/messages') title = 'Messages | Cosen';
+    else if (path === '/onboarding') title = 'Complete Profile | Cosen';
+    else if (path === '/verify-email') title = 'Verify Email | Cosen';
+    else if (path === '/forgot-password' || path.startsWith('/reset-password')) title = 'Reset Password | Cosen';
+    
+    document.title = title;
+  }, [location]);
+
+  return null;
+}
+
 function App() {
   return (
     <Router>
+      <TitleUpdater />
       <div className="flex flex-col min-h-screen">
         <Navbar />
         <main className="flex-grow pb-16 md:pb-0">
